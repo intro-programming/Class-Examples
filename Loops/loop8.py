016ee06489b1f6e2d85da7f87fe4fe4fa7e65996
Denver Risser <denris97@gmail.com>
2018-12-19 21:34:37 -0500

    "done"

---
+++ Loops/loop8.py
@@ -0,0 +1,16 @@
+counter = 8
+
+
+    
+for i in range(counter):
+    
+    if i == 2:
+        print "Hello somebody"
+    
+    if i == 5:
+        print "Hello someboy else"
+    
+    if i == 7:
+        print "Nobody"
+
+    
---
+++ Loops/loop9.py
@@ -0,0 +1,20 @@
+num = int(raw_input("Enter number: "))
+num2 = int(raw_input("Enter another number: "))
+
+
+
+# This loops to make multiplication table
+for bob in range(1,num2+1):
+    print "{} X {} = {}".format(num,bob, num * bob)
+
+for bob in range(1,num2+1):
+    print "{} X {} = {}".format(num,bob, num * bob)
+
+for bob in range(1,num2+1):
+    print "{} X {} = {}".format(num,bob, num * bob)
+
+for bob in range(1,num2+1):
+    print "{} X {} = {}".format(num,bob, num * bob)
+
+for bob in range(1,num2+1):
+    print "{} X {} = {}".format(num,bob, num * bob)
